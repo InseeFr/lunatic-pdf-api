d0bcf4666f9c1bb491859c1158ddd13ead4a297c
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2025-09-18 16:53:42 +0200

    feat: add security header & check URI (#38)

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import swaggerUi from 'swagger-ui-express'
 import pdfRoute from './routes/pdf.route';
 import healthRoute from './routes/health.route';
 import { swaggerOptions } from './config/swagger';
+import helmet from 'helmet';
 
 
 const swaggerDocs = swaggerjsdoc(swaggerOptions)
 
 const app = express();
 app.use(express.json());
+// See: https://helmetjs.github.io/ add http security header
+app.use(helmet());
 
 app.get("/", (_req: Request, res: Response) => {
     res.redirect("/api-docs");
