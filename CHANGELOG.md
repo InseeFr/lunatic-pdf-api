b01839610b13debc30371ba1728cfece575393f4
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2025-12-29 16:39:50 +0100

    fix: wrap tables into multiple pages if necessary (#81)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+- Wrap & split large tables into multiple pages to avoid content overflow
+
 ## [1.2.0] - 2025-11-06
 
 ### Added
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,44 +7,52 @@ import {
   Table as PDFTable,
   TR as PDFTr,
   TD as PDFTd,
+  TH as PDFTh
 } from "@ag-media/react-pdf-table";
 import { styles } from "./styles";
 import { renderContent } from "../utils/markdownParser";
+import { View } from "@react-pdf/renderer";
 
 type Props = LunaticComponentProps<"Table">;
 
 export function Table({ interpret, label, body, header }: Props) {
+
+  const renderHeader = () => (
+    header && (
+      <PDFTh fixed>
+        {header.map((col, x) => (
+          <PDFTd key={x} style={styles.th}>
+            {renderContent(interpret, col.label, styles.headerText)}
+          </PDFTd>
+        ))}
+      </PDFTh>
+    )
+  );
   return (
     <ValueWithLabel interpret={interpret} label={label}>
-      <PDFTable style={styles.table}>
-        {header && (
-          <PDFTr>
-            {header.map((col, x) => (
-              <PDFTd key={x} style={styles.th}>
-                {renderContent(interpret, col.label, styles.headerText)}
-              </PDFTd>
-            ))}
-          </PDFTr>
-        )}
-        {body.map((row, y) => (
-          <PDFTr key={y}>
-            {row.map((col, x) => (
-              <PDFTd key={x} style={styles.td}>
-                {"componentType" in col ? (
-                  <ErrorBoundary
-                    fallback={<article>Error {col.componentType}</article>}
-                    key={x}
-                  >
-                    <LunaticComponent interpret={interpret} component={col} />
-                  </ErrorBoundary>
-                ) : (
-                  renderContent(interpret, col.label, styles.answer)
-                )}
-              </PDFTd>
-            ))}
-          </PDFTr>
-        ))}
-      </PDFTable>
+      <View wrap>
+        <PDFTable style={styles.table}>
+          {renderHeader()}
+          {body.map((row, y) => (
+            <PDFTr key={y} wrap={false}>
+              {row.map((col, x) => (
+                <PDFTd key={x} style={styles.td}>
+                  {"componentType" in col ? (
+                    <ErrorBoundary
+                      fallback={<article>Error {col.componentType}</article>}
+                      key={x}
+                    >
+                      <LunaticComponent interpret={interpret} component={col} />
+                    </ErrorBoundary>
+                  ) : (
+                    renderContent(interpret, col.label, styles.answer)
+                  )}
+                </PDFTd>
+              ))}
+            </PDFTr>
+          ))}
+        </PDFTable>
+      </View>
     </ValueWithLabel>
   );
 }
