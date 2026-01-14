1db4ac72a8e0138f285433e7d38065765aedd4e2
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2026-01-14 15:35:02 +0100

    ref: generate pdf end point (#117)

--- src/controllers/pdf-source-form-data.controller.tsx
+++ src/controllers/pdf-source-form-data.controller.tsx
@@ -1,23 +1,14 @@
-import { Request, Response } from "express";
-import type { LunaticSource } from "@inseefr/lunatic";
+import { Response } from "express";
 import { ErrorCode, handleError } from "../error/api";
 import { readAndValidateLunaticData } from "../services/data-service";
 import { generatePdfStream } from "../services/pdf-service";
-import { PdfRequestFromFormData } from "../types";
+import { PdfRequestFromBody } from "../types";
 
-export const generatePdf = async (req: Request, res: Response) => {
-  const sourceFile = (req as PdfRequestFromFormData).files.source[0];
-  const dataFile = (req as PdfRequestFromFormData).files.data[0];
-
+export const generatePdf = async (req: PdfRequestFromBody, res: Response) => {
+  const requestBody = req.body;
   try {
-    const source: LunaticSource = JSON.parse(
-      sourceFile.buffer.toString("utf-8")
-    );
-
-    const data = readAndValidateLunaticData(
-      res,
-      JSON.parse(dataFile.buffer.toString("utf-8"))
-    );
+    const source = requestBody.source;
+    const data = readAndValidateLunaticData(res, requestBody.interrogation);
     await generatePdfStream(res, source, data);
   } catch (e) {
     return handleError(
